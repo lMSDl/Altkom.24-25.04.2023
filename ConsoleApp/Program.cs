3a46268f5e7b171e96141361ffebe59e10ae451e
Paweł Łęczycki <pawel@msd.info.pl>
2023-04-24 11:55:53 +0200

    Łączenie stringów

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,6 +7,8 @@
         //instrukcje najwyższego poziomu - instrukcje bez jawnego opakowania w funkcję Main
         Console.WriteLine("Hello, World!");
 
+//; - średnik oznacza koniec instrukcji
+
 //deklaracja zmiennej lokalnej o typie string (ciąg znaków) i nazwie helloVariable
 string helloVariable;
 //inicjalizacja zmiennej - pierwsze przypisanie wartości (może być wartością null)
@@ -23,6 +25,34 @@ worldString = "World";
 Console.WriteLine(worldString);
 
 Console.WriteLine(helloVariable + worldString);
+
+Console.Write(helloVariable);
+Console.WriteLine(worldString);
+
+string input = Console.ReadLine();
+Console.WriteLine(input);
+
+
+string helloOutput;
+
+//łączenie stringów za pomocą +
+helloOutput = helloVariable + " " + input;
+Console.WriteLine(helloOutput);
+
+//łączenie stringów za pomocą funkcji Concat
+helloOutput = helloVariable;
+helloOutput = string.Concat(helloOutput, " ", input);
+//helloOutput = string.Concat(helloOutput, input);
+Console.WriteLine(helloOutput);
+
+//łączenie stringów za pomocą funkcji Format
+helloOutput = string.Format("{0} {1}", helloVariable, input);
+Console.WriteLine(helloOutput);
+
+//łączenie stringów wykorzystujhąć interpolację (string interpolowany)
+helloOutput = $"{helloVariable} {input}! :)";
+Console.WriteLine(helloOutput);
+
 
 
 //    }
