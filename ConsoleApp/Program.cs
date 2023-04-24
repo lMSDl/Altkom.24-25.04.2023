2732eec80bf1f7a3981b3c20c53875ad7f1e7a51
Paweł Łęczycki <pawel@msd.info.pl>
2023-04-24 16:01:32 +0200

    tablice i kolekcje

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,109 +6,178 @@
 //    public static void Main()
 //    {
 
+int a = 5;
+int b = 10;
+int c = 15;
 
-int value = int.Parse(Console.ReadLine());
+// tworzymy tablicę z 3 wskazanych elementów
+int[] intArray = {a, b, c};
+//tablica indeksowana jest od 0
+Console.WriteLine(intArray[0]);
 
-// == - porównanie wartości
-if(value == 0)
-{
-    Console.WriteLine($"Wartość {value} jest równa 0");
-}
+//ostatnim elementem tablicy jest indeks n-1, gdzie n to romiar tablicy
+Console.WriteLine(intArray[2]);
 
-// != - różne
-else if (value != 0)
-{
-    Console.WriteLine($"Wartość {value} jest różna od 0");
-}
+Console.WriteLine($"intArray size is {intArray.Length}");
 
+//tworzymy pustą (wypełniona wartościami domyślnymi) tablicę o wielkości 2
+intArray = new int[2];
+Console.WriteLine(intArray[0]);
 
+string[] stringArray = new string[2];
+Console.WriteLine(stringArray[0]);
 
-if (value > 0)
-{
-    Console.WriteLine($"Wartość {value} jest większa 0");
-}
-else 
-{
-    Console.WriteLine($"Wartość {value} jest mniejsza lub równa 0");
-}
-
-Console.WriteLine("---");
-
-// || - logiczne lub
-if (value > 0 || value < 0) {
-    Console.WriteLine($"Wartość {value} jest mniejsza lub większa od 0");
-}
-
-// && - logiczne i
-if (value > 5 && value < 10)
-{
-    Console.WriteLine($"Wartość {value} jest z przedziału (5;10)");
-}
-
-// >= - większe-równe, <= - mniejsze-równe 
-if (value >= 5 && value <= 10)
-{
-    Console.WriteLine($"Wartość {value} jest z przedziału <5;10>");
-}
-
-// ! - negacja
-if (!(value > 5 && value <= 10))
-{
-    Console.WriteLine($"Wartość {value} jest spoza przedziału <5;10>");
-}
-
-
-
-Console.WriteLine("---");
-
-
-if (value > 0)
-{
-    Console.WriteLine($"Wartość {value} jest większa 0");
-}
-else if (value < 0)
-{
-    Console.WriteLine($"Wartość {value} jest mniejsza 0");
-}
-else
-{
-    Console.WriteLine($"Wartość {value} jest równa 0");
-}
-
-//switch przyjmuje parametr, który jest porównywany z listą przypadków
-switch (value)
-{
-    case > 0:
-        Console.WriteLine($"Wartość {value} jest większa 0");
-        break;
-    case < 0:
-        Console.WriteLine($"Wartość {value} jest mniejsza 0");
-        break;
-
-        //default jest opcjonalny i wywoływany gdy nie znaleziono innego dopasowania
-    default:
-        Console.WriteLine($"Wartość {value} jest równa 0");
-        break;
-}
-
-
-Console.WriteLine("Jak masz na imię?");
 string input = Console.ReadLine();
 
-switch(input)
+//tablica jako rezultat wywołania metody
+string[] words = input.Split(" ");
+
+Console.WriteLine(words[0]);
+Console.WriteLine(words[words.Length - 1]);
+
+//znmiana rozmiaru tablicy
+Array.Resize(ref intArray, 10);
+
+string value = words[0];
+stringArray[1] = value;
+stringArray[0] = words[words.Length - 1];
+
+Console.WriteLine(stringArray[1]);
+Console.WriteLine(stringArray[0]);
+
+Array.Sort(words);
+Array.Clear(words);
+
+//utworzenie listy z elementami typu int
+List<int> intList = new List<int>();
+Console.WriteLine($"List size: {intList.Count}");
+//dodawanie pojedynczych wartości do listy
+intList.Add(a);
+intList.Add(c);
+intList.Add(154);
+Console.WriteLine($"List size: {intList.Count}");
+//usuwanie wskazanej wartości
+intList.Remove(15);
+intList.Remove(33);
+Console.WriteLine($"List size: {intList.Count}");
+//usuwanie wartości spod wskazanego indeksu
+intList.RemoveAt(1);
+Console.WriteLine($"List size: {intList.Count}");
+//dodawanie wielu wartości
+intList.AddRange(intArray);
+Console.WriteLine($"List size: {intList.Count}");
+
+Console.WriteLine(intList[0]);
+intList.Sort();
+Console.WriteLine(intList[0]);
+
+
+void Conditions()
 {
-    case "":
-        Console.WriteLine("Szkoda, że nie chcesz podać swojego imienia...");
-        break;
-    case "Alexa":
-        Console.WriteLine("Hej! Masz na imię tak jak asystent Amazona??");
-        break;
-    default:
-        Console.WriteLine($"Cześć {input}! Jak się masz?");
-        break;
+
+    int value = int.Parse(Console.ReadLine());
+
+    // == - porównanie wartości
+    if (value == 0)
+    {
+        Console.WriteLine($"Wartość {value} jest równa 0");
+    }
+
+    // != - różne
+    else if (value != 0)
+    {
+        Console.WriteLine($"Wartość {value} jest różna od 0");
+    }
+
+
+
+    if (value > 0)
+    {
+        Console.WriteLine($"Wartość {value} jest większa 0");
+    }
+    else
+    {
+        Console.WriteLine($"Wartość {value} jest mniejsza lub równa 0");
+    }
+
+    Console.WriteLine("---");
+
+    // || - logiczne lub
+    if (value > 0 || value < 0)
+    {
+        Console.WriteLine($"Wartość {value} jest mniejsza lub większa od 0");
+    }
+
+    // && - logiczne i
+    if (value > 5 && value < 10)
+    {
+        Console.WriteLine($"Wartość {value} jest z przedziału (5;10)");
+    }
+
+    // >= - większe-równe, <= - mniejsze-równe 
+    if (value >= 5 && value <= 10)
+    {
+        Console.WriteLine($"Wartość {value} jest z przedziału <5;10>");
+    }
+
+    // ! - negacja
+    if (!(value > 5 && value <= 10))
+    {
+        Console.WriteLine($"Wartość {value} jest spoza przedziału <5;10>");
+    }
+
+
+
+    Console.WriteLine("---");
+
+
+    if (value > 0)
+    {
+        Console.WriteLine($"Wartość {value} jest większa 0");
+    }
+    else if (value < 0)
+    {
+        Console.WriteLine($"Wartość {value} jest mniejsza 0");
+    }
+    else
+    {
+        Console.WriteLine($"Wartość {value} jest równa 0");
+    }
+
+    //switch przyjmuje parametr, który jest porównywany z listą przypadków
+    switch (value)
+    {
+        case > 0:
+            Console.WriteLine($"Wartość {value} jest większa 0");
+            break;
+        case < 0:
+            Console.WriteLine($"Wartość {value} jest mniejsza 0");
+            break;
+
+        //default jest opcjonalny i wywoływany gdy nie znaleziono innego dopasowania
+        default:
+            Console.WriteLine($"Wartość {value} jest równa 0");
+            break;
+    }
+
+
+    Console.WriteLine("Jak masz na imię?");
+    string input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "":
+            Console.WriteLine("Szkoda, że nie chcesz podać swojego imienia...");
+            break;
+        case "Alexa":
+            Console.WriteLine("Hej! Masz na imię tak jak asystent Amazona??");
+            break;
+        default:
+            Console.WriteLine($"Cześć {input}! Jak się masz?");
+            break;
+    }
+
 }
-
-
 
 
 void Numbers()
