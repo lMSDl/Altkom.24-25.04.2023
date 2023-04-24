04653f01ca649cb5c5915ec0860a438f51450c7f
Paweł Łęczycki <pawel@msd.info.pl>
2023-04-24 15:11:39 +0200

    if

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,95 +6,150 @@
 //    public static void Main()
 //    {
 
-int a = 11;
-int b = 4;
 
-int c = a + b;
-Console.WriteLine($"{a} + {b} = {c}");
-c = a - b;
-Console.WriteLine($"{a} - {b} = {c}");
-c = a * b;
-Console.WriteLine($"{a} * {b} = {c}");
-c = a / b; // część dziesiętna jest ucinana
-Console.WriteLine($"{a} / {b} = {c}");
-c = a % b; // reszta z dzielenia
-Console.WriteLine($"{a} % {b} = {c}");
+int value = int.Parse(Console.ReadLine());
 
+// == - porównanie wartości
+if(value == 0)
+{
+    Console.WriteLine($"Wartość {value} jest równa 0");
+}
 
-float aa = 11f;
-float bb = 4.0f;
-float cc = aa + bb;
-Console.WriteLine($"{aa} + {bb} = {cc}");
-cc = aa - bb;
-Console.WriteLine($"{aa} - {bb} = {cc}");
-cc = aa * bb;
-Console.WriteLine($"{aa} * {bb} = {cc}");
-cc = aa / bb;
-Console.WriteLine($"{aa} / {bb} = {cc}");
-
-// kolejność działań zgodna z zasadami matematyki
-cc = 1f * a / b;
-Console.WriteLine($"{a} / {bb} = {cc}");
-
-c = a + a * a;
-Console.WriteLine($"{a} + {a} * {a} = {c}");
-c = (a + a) * a;
-Console.WriteLine($"({a} + {a}) * {a} = {c}");
-
-
-Console.WriteLine($"short min:{short.MinValue} max:{short.MaxValue}");
-Console.WriteLine($"int min:{int.MinValue} max:{int.MaxValue}");
-Console.WriteLine($"long min:{long.MinValue} max:{long.MaxValue}");
-
-Console.WriteLine($"float min:{float.MinValue} max:{float.MaxValue}");
-Console.WriteLine($"double min:{double.MinValue} max:{double.MaxValue}");
-Console.WriteLine($"decimal min:{decimal.MinValue} max:{decimal.MaxValue}");
-
-//dzielenie przez float/double/decimal daje wynik o wyższej precyzji
-Console.WriteLine(5 / 3.3f);
-Console.WriteLine(5 / 3.3d);
-Console.WriteLine(5 / 3.3m);
-
-//zaokrąglanie domyślnie wykorzustyje "financial rounding"
-Console.WriteLine(Math.Round(2.5));
-Console.WriteLine(Math.Round(3.5));
-
-//zaokrąglanie klasyczne
-Console.WriteLine(Math.Round(3.5, MidpointRounding.AwayFromZero));
+// != - różne
+else if (value != 0)
+{
+    Console.WriteLine($"Wartość {value} jest różna od 0");
+}
 
 
 
-int intMax = int.MaxValue;
-long longMax = long.MaxValue;
+if (value > 0)
+{
+    Console.WriteLine($"Wartość {value} jest większa 0");
+}
+else 
+{
+    Console.WriteLine($"Wartość {value} jest mniejsza lub równa 0");
+}
+
+Console.WriteLine("---");
+
+// || - logiczne lub
+if (value > 0 || value < 0) {
+    Console.WriteLine($"Wartość {value} jest mniejsza lub większa od 0");
+}
+
+// && - logiczne i
+if (value > 5 && value < 10)
+{
+    Console.WriteLine($"Wartość {value} jest z przedziału (5;10)");
+}
+
+// >= - większe-równe, <= - mniejsze-równe 
+if (value >= 5 && value <= 10)
+{
+    Console.WriteLine($"Wartość {value} jest z przedziału <5;10>");
+}
+
+// ! - negacja
+if (!(value > 5 && value <= 10))
+{
+    Console.WriteLine($"Wartość {value} jest spoza przedziału <5;10>");
+}
 
 
-//longMax = intMax;
-//chcąć przypisać wartość o wyższej precyzji do zmiennej o niższej musimy zastosować rzutowanie
-intMax = (int)longMax;
-Console.WriteLine(intMax);
+void Numbers()
+{
 
-float floatValue = int.MaxValue;
+    int a = 11;
+    int b = 4;
 
-Console.WriteLine(floatValue);
+    int c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+    c = a / b; // część dziesiętna jest ucinana
+    Console.WriteLine($"{a} / {b} = {c}");
+    c = a % b; // reszta z dzielenia
+    Console.WriteLine($"{a} % {b} = {c}");
 
-floatValue = (float)double.MaxValue;
-Console.WriteLine(floatValue);
+
+    float aa = 11f;
+    float bb = 4.0f;
+    float cc = aa + bb;
+    Console.WriteLine($"{aa} + {bb} = {cc}");
+    cc = aa - bb;
+    Console.WriteLine($"{aa} - {bb} = {cc}");
+    cc = aa * bb;
+    Console.WriteLine($"{aa} * {bb} = {cc}");
+    cc = aa / bb;
+    Console.WriteLine($"{aa} / {bb} = {cc}");
+
+    // kolejność działań zgodna z zasadami matematyki
+    cc = 1f * a / b;
+    Console.WriteLine($"{a} / {bb} = {cc}");
+
+    c = a + a * a;
+    Console.WriteLine($"{a} + {a} * {a} = {c}");
+    c = (a + a) * a;
+    Console.WriteLine($"({a} + {a}) * {a} = {c}");
 
 
-//parsowanie/konwersja string na int
-string value = Console.ReadLine();
-int intVal = int.Parse(value);
-int result = intVal * 2;
+    Console.WriteLine($"short min:{short.MinValue} max:{short.MaxValue}");
+    Console.WriteLine($"int min:{int.MinValue} max:{int.MaxValue}");
+    Console.WriteLine($"long min:{long.MinValue} max:{long.MaxValue}");
 
-Console.WriteLine(  $"{intVal} * 2 = {result}");
+    Console.WriteLine($"float min:{float.MinValue} max:{float.MaxValue}");
+    Console.WriteLine($"double min:{double.MinValue} max:{double.MaxValue}");
+    Console.WriteLine($"decimal min:{decimal.MinValue} max:{decimal.MaxValue}");
 
-//parsowanie/konwersja string na float
-value = Console.ReadLine();
-float floatVal = float.Parse(value);
-float floatResult = floatVal * 2;
+    //dzielenie przez float/double/decimal daje wynik o wyższej precyzji
+    Console.WriteLine(5 / 3.3f);
+    Console.WriteLine(5 / 3.3d);
+    Console.WriteLine(5 / 3.3m);
 
-Console.WriteLine($"{floatVal} * 2 = {floatResult}");
+    //zaokrąglanie domyślnie wykorzustyje "financial rounding"
+    Console.WriteLine(Math.Round(2.5));
+    Console.WriteLine(Math.Round(3.5));
 
+    //zaokrąglanie klasyczne
+    Console.WriteLine(Math.Round(3.5, MidpointRounding.AwayFromZero));
+
+
+
+    int intMax = int.MaxValue;
+    long longMax = long.MaxValue;
+
+
+    //longMax = intMax;
+    //chcąć przypisać wartość o wyższej precyzji do zmiennej o niższej musimy zastosować rzutowanie
+    intMax = (int)longMax;
+    Console.WriteLine(intMax);
+
+    float floatValue = int.MaxValue;
+
+    Console.WriteLine(floatValue);
+
+    floatValue = (float)double.MaxValue;
+    Console.WriteLine(floatValue);
+
+
+    //parsowanie/konwersja string na int
+    string value = Console.ReadLine();
+    int intVal = int.Parse(value);
+    int result = intVal * 2;
+
+    Console.WriteLine($"{intVal} * 2 = {result}");
+
+    //parsowanie/konwersja string na float
+    value = Console.ReadLine();
+    float floatVal = float.Parse(value);
+    float floatResult = floatVal * 2;
+
+    Console.WriteLine($"{floatVal} * 2 = {floatResult}");
+}
 
 
 void Strings()
