c6de3b7bfaba923f5911bfee13fef5c8dcae6b03
Paweł Łęczycki <pawel@msd.info.pl>
2023-04-25 14:28:16 +0200

    ShoppingList

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using ShoppingList.Models;
+using ShoppingList.Services;
+
+ProductsService productsService = new ProductsService();
+
+bool exit = false;
+
+do
+{
+    ShowList(productsService);
+
+    int option = ReadInt();
+
+    switch (option)
+    {
+        case 1:
+            AddNewProduct(productsService);
+            break;
+        case 2:
+            RemoveProduct(productsService);
+            break;
+        case 3:
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Błędna opcja..");
+            break;
+    }
+} while (!exit);
+
+
+int ReadInt()
+{
+    int value;
+    bool parseResult;
+    do
+    {
+        string input = Console.ReadLine();
+        parseResult = int.TryParse(input, out value);
+        if (parseResult == false)
+        {
+            Console.WriteLine("Błędne dane");
+        }
+    } while (!parseResult);
+
+    return value;
+}
+
+void AddNewProduct(ProductsService productsService)
+{
+    Product product = new Product();
+
+    Console.Write("Podaj nazwę: ");
+    product.Name = Console.ReadLine();
+    Console.Write("Podaj ilość: ");
+    product.Quantity = ReadInt();
+    productsService.Add(product);
+}
+
+void RemoveProduct(ProductsService productsService)
+{
+    Console.Write("Podaj pozycję do usunięcia: ");
+    int position = ReadInt();
+    productsService.Remove(position - 1);
+}
+
+void ShowList(ProductsService productsService)
+{
+    Console.Clear();
+    productsService.Show();
+    Console.WriteLine();
+    Console.WriteLine($"1. Dodaj   2. Usuń   3. Koniec");
+}
---
+++ ShoppingList/Services/ProductsService.cs
@@ -0,0 +1,32 @@
+﻿using ShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Services
+{
+    public class ProductsService
+    {
+        private readonly List<Product> products = new List<Product>();
+    
+        public void Show()
+        {
+            for (int i = 0; i < products.Count; i++) {
+                Product? product = products[i];
+                Console.WriteLine($"{i+1}. {product.Name} x{product.Quantity}");
+            }
+        }
+
+        public void Add(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void Remove(int position) 
+        { 
+            products.RemoveAt(position);
+        }
+    }
+}
